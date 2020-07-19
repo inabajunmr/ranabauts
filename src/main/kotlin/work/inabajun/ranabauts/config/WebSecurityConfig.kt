6a0c8b7d1d74cd5b9dce757bb8c15317bd279f57
inabajunmr <inabajun.for.regi@gmail.com>
2020-07-19 16:24:18 +0900

    add spring actuator for health check

--- src/main/kotlin/work/inabajun/ranabauts/config/WebSecurityConfig.kt
+++ src/main/kotlin/work/inabajun/ranabauts/config/WebSecurityConfig.kt
@@ -3,12 +3,18 @@ package work.inabajun.ranabauts.config
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.Environment
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 @Configuration
 @EnableWebSecurity
 class WebSecurityConfig(val env: Environment) : WebSecurityConfigurerAdapter() {
+
+    @Throws(Exception::class)
+    override fun configure(web: WebSecurity) {
+        web.ignoring().antMatchers("/actuator/**")
+    }
 
     // for basic auth
     override fun configure(http: HttpSecurity) {
